9170cbc049e19b3867f63dcf7def92cf4302ba09
Sasha Varlamov <sasha@sashavarlamov.com>
2015-09-12 17:47:34 +0800

    Implementations and Benchmarks

---
+++ gork_test.go
@@ -0,0 +1,43 @@
+package gork
+
+import (
+	"testing"
+)
+
+const n = 16
+
+func BenchmarkRunes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RandStringRunes(n)
+	}
+}
+
+func BenchmarkBytes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RandStringBytes(n)
+	}
+}
+
+func BenchmarkBytesRmndr(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RandStringBytesRmndr(n)
+	}
+}
+
+func BenchmarkBytesMask(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RandStringBytesMask(n)
+	}
+}
+
+func BenchmarkBytesMaskImpr(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RandStringBytesMaskImpr(n)
+	}
+}
+
+func BenchmarkBytesMaskImprSrc(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RandStringBytesMaskImprSrc(n)
+	}
+}
